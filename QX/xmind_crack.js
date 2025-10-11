e94df9d572c51040b89a73c40318d861301751d3
36henan <90303938+HenanLee@users.noreply.github.com>
2025-10-11 15:53:08 +0800

    创建 xmind_crack.js

---
+++ QX/xmind_crack.js
@@ -0,0 +1,17 @@
+/*
+脚本作者：chxm1023
+脚本日期：2023-08-19
+引用地址：https://raw.githubusercontent.com/chxm1023/Rewrite/main/Xmind.js
+*/
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023 = {
+  "license" : {
+    "status" : "sub",
+    "expireTime" : 4092599349000
+  },
+  "_code" : 200,
+  "raw_data" : "bKnTeMMu/TJ86ZFoUqG+sfKC1nitD8b/NaUT93UBc0woo0BF5uBVYXsHUQJkQxLHGlZCbdnKaKzZ4iljM9xddkyMycpUbAtDoywnr4RNzxqXY20pDM0Bdgm+HssaS1RworH8aaaf0IYnnGplxIuqMyT9107PrenF0MBUIaKU9Cw="
+};
+
+$done({body : JSON.stringify(chxm1023)});
