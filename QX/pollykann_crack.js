1add560aef9934acf68c957cacd9f8ed6f848bc0
36henan <90303938+HenanLee@users.noreply.github.com>
2025-10-20 12:51:15 +0800

    Update pollykann_crack.js

--- QX/pollykann_crack.js
+++ QX/pollykann_crack.js
@@ -5,7 +5,7 @@
 [rewrite_local]
 
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/itunes/pollykann.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/HenanLee/henanBox/refs/heads/main/QX/pollykann_crack.js
 
 [mitm] 
 
